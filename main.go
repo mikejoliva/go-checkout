1e7d8197305f2f0ecb73a6b55f51f581b1449881
Michael Oliva <mikejoliva@gmail.com>
2026-03-17 15:30:32 +0000

    feat: implement main
    - Allow the project to be run from a CLI.

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"path"
 
+	"github.com/mikejoliva/go-checkout/internal/checkout"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Expected exactly 1 argument with SKU code(s)!\n")
+		os.Exit(1)
+	}
+
+	checkout, err := checkout.NewCheckout(path.Join("config", "stocklist.yaml"))
+	if err != nil {
+		fmt.Printf("Failed to create checkout: %s\n", err)
+		os.Exit(1)
+	}
+
+	product_codes := os.Args[1]
+	for _, code := range product_codes {
+		err = checkout.Scan(string(code))
+		if err != nil {
+			fmt.Printf("Failed to scan code %s: %s\n", string(code), err)
+			os.Exit(1)
+		}
+	}
+
+	total, err := checkout.GetTotalPrice()
+	if err != nil {
+		fmt.Printf("Failed to calculate total price: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Total price: %d\n", total)
 }
